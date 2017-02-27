a67ec781d97bbb390c9122632427232c791537fa
JeanLLopes <jean.llopes@outlook.com>
2017-02-27 14:05:13 -0300

    Apply Module

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+//PASS 1
 // src/app.js
+// const root = document.querySelector('#root')
+// root.innerHTML = `<p>Hello webpack.</p>`
+
+
+// src/app.js
+import {groupBy} from 'lodash/collection'
+
+//IMPORT THIS MODULE TO people.js
+import people from './people'
+
+const managerGroups = groupBy(people, 'manager')
+debugger;
 const root = document.querySelector('#root')
-root.innerHTML = `<p>Hello webpack.</p>`
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
